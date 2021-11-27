75708130aafb1eb17e01c799160b09eac6140dc1
An0n1mity <49009949+An0n1mity@users.noreply.github.com>
2021-11-27 20:44:10 +0100

    Add files via upload

--- svgparser.h
+++ svgparser.h
@@ -4,27 +4,30 @@
 #include <libxml/tree.h>
 #include <libxml/parser.h>
 #include <stdio.h>
+#include <string.h>
 
-typedef struct xmlDoc* svgTree;
-typedef struct xmlNode svgNode;
+typedef struct xmlNodeShape_s xmlNodeShape;
+
+struct xmlNodeShape_s {
+	xmlNode* shape;
+	struct xmlNodeShape_s* next;
+};
 
 /**
 * @brief Load the svg in memory as a tree
 * \param[in] svg_path path to svg file
 * \return svgTree
 */
-svgTree PARSER_LoadSVG(char* svg_path);
+xmlDocPtr PARSER_LoadSVG(char* svg_path);
 /**
 * @brief Free the svg tree in memory
 * \param[in] svg_tree svg tree to free
 */
-void PARSER_FreeSVG(svgTree svg_tree);
-
-void PARSER_PrintNames(xmlNode* svg_node);
+void PARSER_FreeSVG(xmlDocPtr svg_tree);
+/**
+* @brief Get nodes containing shapes
+*/
+xmlNodeShape* PARSER_GetShapesFromSVG(xmlDocPtr svg_tree);
+void PARSER_ReadShapesFromSVG(xmlNodeShape** shapes, xmlNode* svg_node);
+void PARSER_AddShapesNode(xmlNodeShape** shapes_node, xmlNode* node_to_add);
 #endif // !PARSER_H
-
-
-
-
-
-
