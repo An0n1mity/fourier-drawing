fc9b5c656f273b83b304c9565ed9ef18612e7aec
Blue-Widge <59555837+Blue-Widge@users.noreply.github.com>
2021-12-07 22:59:25 +0100

    cbo
    Rendering of bezier curve

--- Cycloide/main.c
+++ Cycloide/main.c
@@ -1,23 +1,81 @@
 #include "shapes.h"
 #include "svgparser.h"
 #include "BezierCurve.h"
+#include "SDL2/SDL.h"
 
-int main()
+int precision = 1;
+
+void doInput()
+{
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				exit(0);
+				break;
+			case SDLK_UP:
+				precision++;
+				break;
+			case SDLK_DOWN:
+				precision -= precision > 1 ? 1 : 0;
+				break;
+			case SDL_QUIT:
+				exit(0);
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+}
+
+int main(int argc, char* argv[])
 {
 	xmlDocPtr svgfile = PARSER_LoadSVG("../Cycloide/test.xml");
 	xmlNodeShape* shapes = PARSER_GetShapesFromSVG(svgfile);
 
-	SHAPE_Point test[4];
-	test[0].x = 0; test[0].y = 0;
-	test[1].x = 0.2; test[1].y = 0.4;
-	test[2].x = 0.8; test[2].y = 0.4;
-	test[3].x = 1; test[3].y = 1;
-	int** func = getBezierFunction(test[0], test[1], test[2], test[3]);
+	SHAPE_Point test[4], testResult[4];
+	test[0].x = 50; test[0].y = 50;
+	test[1].x = 300; test[1].y = 10;
+	test[2].x = 400; test[2].y = 600;
+	test[3].x = 1000; test[3].y = 500;
+	double** func = getBezierFunction(test[0], test[1], test[2], test[3]);
 
-	test[0] = getBezierPoint(func, 0.25);
-	test[1] = getBezierPoint(func, 0.5);
-	test[2] = getBezierPoint(func, 0.75);
-	test[3] = getBezierPoint(func, 1);
-	printf("en 1/4 : (%lf;%lf)\n en 1/2 : (%lf;%lf)\n en 3/4 : (%lf;%lf)\n en 1 : (%lf;%lf)\n", test[0].x, test[0].y, test[1].x,test[1].y,test[2].x, test[2].y, test[3].x,test[3].y);
+	testResult[0] = getBezierPoint(func, 0.25);
+	testResult[1] = getBezierPoint(func, 0.5);
+	testResult[2] = getBezierPoint(func, 0.75);
+	testResult[3] = getBezierPoint(func, 1);
+
+	SDL_Window* window = SDL_CreateWindow("BezierCurve", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 720, SDL_WINDOW_OPENGL);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+	while (1)
+	{
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		doInput();
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderDrawLineF(renderer, test[0].x, test[0].y, test[1].x, test[1].y);
+		SDL_RenderDrawLineF(renderer, test[1].x, test[1].y, test[2].x, test[2].y);
+		SDL_RenderDrawLineF(renderer, test[2].x, test[2].y, test[3].x, test[3].y);
+
+		SDL_SetRenderDrawColor(renderer, 0, 100, 255, 255);
+		SHAPE_Point lastPoint = test[0];
+		for (int i = 0; i <= precision; ++i)
+		{
+			SHAPE_Point current = getBezierPoint(func, (double)i/(double)precision);
+			SDL_RenderDrawLineF(renderer, lastPoint.x, lastPoint.y, current.x, current.y);
+			lastPoint = current;
+		}
+		SDL_RenderPresent(renderer);
+		SDL_Delay(0);
+	}
+
 	freeBezierFunction(func);
 }
