e099b31d5ddb9c441ee28f52d16627c4fd9ca1ab
Cmooi <lorget@et.esiea.fr>
2022-03-15 18:44:48 +0100

    structure potable

--- Cycloide/MenuTextures.c
+++ Cycloide/MenuTextures.c
@@ -8,19 +8,18 @@
 
 void Create(MenuTextures **textures, SDL_Renderer* renderer, char* path,char* name, int x, int y, int h, int w) {
 
-    int a = 0;
-    int b = 0;
-    if ((*textures)->image == NULL)b = 1;
 
-    while ((textures != NULL) || b==1) {
-        textures = (*textures)->m_next;
-        a++;
+
+    while ((*textures)->name != NULL) {
+        (*textures)->m_next = (MenuTextures*)calloc(1, sizeof(MenuTextures));
+        (*textures)->m_next->image = (Image*)calloc(1, sizeof(Image));
+         textures = (*textures)->m_next;
+       
     }
-    if(a!=0)
-        textures = (MenuTextures*)calloc(1, sizeof(MenuTextures));
-
    
-    (*textures)->name = "stp";
+   
+   
+    (*textures)->name = name;
     (*textures)->image = (Image*)calloc(1, sizeof(Image));
 
     (*textures)->image->s = IMG_Load(path);
@@ -32,29 +31,29 @@ void Create(MenuTextures **textures, SDL_Renderer* renderer, char* path,char* na
     (*textures)->image->r->y = y;
     (*textures)->image->r->h = h;
     (*textures)->image->r->w = w;
-        
+
+   
 }
 
 
 
-MenuTextures* MenuTextures_new(SDL_Renderer* renderer) {
-    MenuTextures* textures = (MenuTextures*)calloc(1, sizeof(MenuTextures));
+MenuTextures** MenuTextures_new(SDL_Renderer* renderer) {
+    MenuTextures** textures = (MenuTextures**)calloc(1, sizeof(MenuTextures*));
+    (*textures)->image = (Image*)calloc(1, sizeof(Image));
 
 
+    Create(textures, renderer, "../Assets/Images/Menu/rouleau.png","rouleau", 330, 0, 720, 50);
 
-    Create(&textures, renderer, "../Assets/Images/Menu/rouleau.png","rouleau", 330, 0, 720, 50);
+    Create(textures, renderer, "../Assets/Images/Menu/black_background.png", "background", 0, 0, 720, 330);
 
-    Create(&textures, renderer, "../Assets/Images/Menu/black_background.png", "background", 0, 0, 720, 330);
-
-    Create(&textures, renderer, "../Assets/Images/Menu/point.png","point", 10, 100, 100, 100);
-
-    Create(&textures, renderer, "../Assets/Images/Menu/cercle.png","cercle", 130, 100, 100, 100);
+    Create(textures, renderer, "../Assets/Images/Menu/point.png","point", 10, 100, 100, 100);
 
 
-    Create(&textures, renderer, "../Assets/Images/Menu/segment.png","segment", 10, 210, 100, 100);
+    Create(textures, renderer, "../Assets/Images/Menu/segment.png","segment", 10, 210, 100, 100);
+
+    Create(textures, renderer, "../Assets/Images/Menu/cercle.png","cercle", 130, 100, 100, 100);
 
 
-    
 
     return textures;
 }
