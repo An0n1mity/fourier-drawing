cfd2adb917ec9d326d8da0dfcd855fae8276deab
Cmooi <94480778+Cmooi@users.noreply.github.com>
2022-04-22 22:54:04 +0200

    Changement structure et sa marche
    total abandont des listes adjacentes, on en reparle plus ok ? Du coup j ai juste fais une liste d'image statique.
    Sa change pas beaucoup sur le résultat mais bcp plus facile a faire !

--- Cycloide/MenuTextures.c
+++ Cycloide/MenuTextures.c
@@ -6,28 +6,24 @@
 #include "SDL_image.h"
 
 
-MenuTextures* CreateMenuTexture(SDL_Renderer* renderer, char* path, char* name, int x, int y, int h, int w) {
-    MenuTextures* texture = (MenuTextures*)calloc(1, sizeof(MenuTextures));
-    texture->image = (Image*)calloc(1, sizeof(Image));
-    texture->image->r = (SDL_Rect*)calloc(1, sizeof(SDL_Rect));
-
-    int i = 0;
-    while (name[i]) {
-        texture->name[i] = name[i];
-        i++;
-    }
-
-    texture->image->s = IMG_Load(path);
-    texture->image->t = SDL_CreateTextureFromSurface(renderer, texture->image->s);
-    SDL_FreeSurface(texture->image->s);
-
-    texture->image->r->x = x;
-    texture->image->r->y = y;
-    texture->image->r->h = h;
-    texture->image->r->w = w;
-
-    return texture;
+MenuTextures* MenuTextures_new(SDL_Renderer* renderer) {
+    MenuTextures* textures = (MenuTextures*)  calloc(1, sizeof(MenuTextures));
+    textures->nbImage = 0;
     
+
+    Create_Menu(&textures, renderer, "../Assets/Images/Menu/rouleau.png", "rouleau", 330, 0, 720, 50);
+
+    Create_Menu(&textures, renderer, "../Assets/Images/Menu/black_background.png", "background", 0, 0, 720, 330);
+
+    Create_Menu(&textures, renderer, "../Assets/Images/Menu/point.png", "point", 10, 100, 100, 100);
+
+    Create_Menu(&textures, renderer, "../Assets/Images/Menu/segment.png", "segment", 10, 210, 100, 100);
+
+    Create_Menu(&textures, renderer, "../Assets/Images/Menu/cercle.png", "cercle", 130, 100, 100, 100);
+
+
+
+    return textures;
 }
 
 void Create_Menu(MenuTextures** textures, SDL_Renderer* renderer, char* path, char* name, int x, int y, int h, int w)
@@ -40,49 +36,50 @@ void Create_Menu(MenuTextures** textures, SDL_Renderer* renderer, char* path, ch
     }
 
     // Stack adding
-    MenuTextures* texture_to_add = CreateMenuTexture(renderer, path, name, x, y, h, w);
-    texture_to_add->m_next = *textures;
-    *textures = texture_to_add;
+    Image* texture_to_add = CreateMenuTexture(renderer, path, name, x, y, h, w);
+
+    (*textures)->list[(*textures)->nbImage] = texture_to_add;
+    (*textures)->nbImage += 1;
+    
 }
 
-MenuTextures* MenuTextures_new(SDL_Renderer* renderer) {
-    MenuTextures* textures = NULL;
-    
-    
-    Create_Menu(&textures, renderer, "../Assets/Images/Menu/rouleau.png","rouleau", 330, 0, 720, 50);
+Image* CreateMenuTexture(SDL_Renderer* renderer, char* path, char* name, int x, int y, int h, int w) {
+    Image* texture = (Image*)calloc(1, sizeof(Image));
 
-    Create_Menu(&textures, renderer, "../Assets/Images/Menu/black_background.png", "background", 0, 0, 720, 330);
+    texture->r = (SDL_Rect*)calloc(1, sizeof(SDL_Rect));
 
-    Create_Menu(&textures, renderer, "../Assets/Images/Menu/point.png","point", 10, 100, 100, 100);
-
-    Create_Menu(&textures, renderer, "../Assets/Images/Menu/segment.png","segment", 10, 210, 100, 100);
-
-    Create_Menu(&textures, renderer, "../Assets/Images/Menu/cercle.png","cercle", 130, 100, 100, 100);
-
-
-
-    return textures;
-}
-
-void Printf(MenuTextures* textures) {
-    if (!textures)return;
-
-    while (textures) {
-        //printf("%s \n", textures->name);
-        textures = textures->m_next;
+    int i = 0;
+    while (name[i]) {
+        texture->name[i] = name[i];
+        i++;
     }
 
+    texture->s = IMG_Load(path);
+    texture->t = SDL_CreateTextureFromSurface(renderer, texture->s);
+    SDL_FreeSurface(texture->s);
+
+    texture->r->x = x;
+    texture->r->y = y;
+    texture->r->h = h;
+    texture->r->w = w;
+
+
+    
+
+    return texture;
+
 }
 
-MenuTextures* Found(MenuTextures* textures, char* name) {
+int Found(MenuTextures* texture, char* name) {
 
-    if (!textures)return NULL;
-
-    while (name != textures->name) {
-        textures = textures->m_next;
+    if (!texture)return NULL;
+    int i = 0;
+    for (i; i <= texture->nbImage; i++) {
+        if (name = texture->list[i]->name) return i;
     }
+    
 
-    return textures;
+    return -1;
 }
 
 void Image_free(Image* im) {
@@ -94,29 +91,27 @@ void Image_free(Image* im) {
    
 }
 
-void render(SDL_Renderer* renderer, MenuTextures* textures) {
+void render(SDL_Renderer* renderer, MenuTextures* texture) {
 
-    if (textures == NULL)return;
-
-    while (textures) {
-        SDL_RenderCopy(renderer, textures->image->t, NULL, textures->image->r);
-        textures = textures->m_next;
+    if (texture == NULL)return;
+    
+    for (int i = 0; i < texture->nbImage; i++) {
+        SDL_RenderCopy(renderer, texture->list[i]->t, NULL, texture->list[i]->r);
     }
+    SDL_RenderPresent(renderer);
 }
 
-void MenuTextures_free(MenuTextures* textures) {
+void MenuTextures_free(MenuTextures* texture) {
 
-    if (!textures)
+    if (!texture)
         return;
-    MenuTextures* tmp = textures->m_next;
-    while (textures) {
-        Image_free(textures->image);
-        free(textures);
-        textures = tmp;
-        tmp = tmp->m_next;
+
+    for (int i = 0; i <= texture->nbImage; i++) {
+        Image_free(texture->list[i]);
+        
     }
 
-    free(textures);
+    free(texture);
 }
 
 
--- Cycloide/MenuTextures.h
+++ Cycloide/MenuTextures.h
@@ -10,9 +10,10 @@ typedef struct Image_s
     SDL_Texture* t;
     SDL_Surface* s;
     SDL_Rect* r;
+    char* name[128];
 
 }Image;
-
+/*
 typedef struct Textures_s
 {
     int b;
@@ -28,9 +29,30 @@ typedef struct MenuTextures_s
 
 } MenuTextures;
 
+typedef struct Textures_s
+{
+    Image* image;
+    char* name[128];
+    struct MenuTextures* m_next;
+
+}Textures;
+
+typedef struct MenuTextures_s
+{
+    Textures* begin;
+
+} MenuTextures;
+*/
+typedef struct MenuTextures_s
+{
+    int nbImage;
+    Image* list[5];
+
+} MenuTextures;
+
 
 void Create_Menu(MenuTextures** textures, SDL_Renderer* renderer, char* path,char *name, int x, int y, int h, int w);
-MenuTextures* CreateMenuTexture(SDL_Renderer*  renderer, char*  path, char*  name, int x, int y, int h, int w);
+Image* CreateMenuTexture(SDL_Renderer*  renderer, char*  path, char*  name, int x, int y, int h, int w);
 
 
 MenuTextures* MenuTextures_new(SDL_Renderer* renderer);
@@ -38,4 +60,4 @@ void MenuTextures_free(MenuTextures* textures);
 void Image_free(Image* im);
 void Deroulement(MenuTextures* textures, bool rouleau, bool rouleau2);
 
-MenuTextures* Found(MenuTextures* textures, char *name);
+int Found(MenuTextures* textures, char *name);
