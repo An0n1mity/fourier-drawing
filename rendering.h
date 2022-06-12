895179aea184228accba899c2bc09169010b672a
An0n1mity <49009949+An0n1mity@users.noreply.github.com>
2022-06-12 17:35:50 +0200

    Delete rendering.h

--- rendering.h
+++
@@ -1,7 +0,0 @@
-#include <SDL.h>
-#include "shapes.h"
-
-// TODO : Rendering a Path 
-void RENDERER_DrawPath(SDL_Renderer* renderer, ShapePath* path);
-
-void RENDERER_DrawPoints(SDL_Renderer* renderer, ShapePoint* points);
