71b1ef02f0ae093f3feff51b7cc02be519279b2b
An0n1mity <49009949+An0n1mity@users.noreply.github.com>
2022-06-12 20:22:25 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
+
+#define SDL_MAIN_HANDLED
+
 #include "shapes.h"
 #include "svgparser.h"
 #include "GUI.h"
-#include <inttypes.h>
 #include "fft.h"
 #include "audio.h"
 
+#define gtk
+
 int main(int argc, char** argv) {
-    // Get the sets of points from the abstract shapes this operation frees the abstract shapes stack
-    size_t nb_points = 0;
-    ShapePoint *points = SHAPE_GetPointsFromAbstractShapes(abstract_shapes, 0.1f, &nb_points);
-    kiss_fft_cpx* complex_array = GetComplexArrayFromPoints(points, nb_points);
-    struct FFT_s* fft = GetFFTOfComplexArray(complex_array, nb_points);
-    
     Pa_Initialize();
 
     PaStream* stream;
@@ -36,8 +34,6 @@ int main(int argc, char** argv) {
               paStreamCallback,
               &data );
 
-    Pa_StartStream(stream);
-
     gtk_init(&argc, &argv);
 
     GtkBuilder* builder = gtk_builder_new ();
@@ -48,25 +44,33 @@ int main(int argc, char** argv) {
     user_data.pa_data = &data;
 
     gtk_builder_add_from_file (builder, "../gui.glade", &error);
-    if(error)
-    {
-        g_error ("%s", error->message);
-    }
+  
     GtkWidget * window = (GtkWidget *) gtk_builder_get_object (builder, "window");
     GtkFileChooserButton* file_chooser_button = (GtkFileChooserButton *) gtk_builder_get_object (builder, "file_chooser");
     GtkScale* precision_scale = (GtkScale *) gtk_builder_get_object (builder, "precision_scale");
     GtkDrawingArea* drawing_area = (GtkDrawingArea *) gtk_builder_get_object (builder, "drawing_area");
     GtkCheckButton* svg_check_button = (GtkCheckButton *) gtk_builder_get_object (builder, "svg_check_button");
+    GtkCheckButton* frequency_check_button = (GtkCheckButton*) gtk_builder_get_object(builder, "frequency_check_button");
     GtkButton* screenshot_button = (GtkButton*) gtk_builder_get_object (builder, "screenshot_button");
+    GtkButton* cycloides_check_button = (GtkButton*) gtk_builder_get_object(builder, "cycloides_check_button");
     user_data.drawing_area = drawing_area;
 
-    g_timeout_add (10 /* milliseconds */, ForceRenderUpdate, &user_data);
+    g_timeout_add (10, ForceRenderUpdate, &user_data);
     g_signal_connect (G_OBJECT (file_chooser_button), "selection-changed", G_CALLBACK (GetFileNameFromFileChooser), &user_data);
     g_signal_connect(G_OBJECT(precision_scale), "value_changed", G_CALLBACK( GetPrecisionFromScale), &user_data);
     g_signal_connect(G_OBJECT(drawing_area), "draw", G_CALLBACK(DrawOnScreen), &user_data);
     g_signal_connect(G_OBJECT(svg_check_button), "clicked", G_CALLBACK(SVGCheckButton), &user_data);
+    g_signal_connect(G_OBJECT(frequency_check_button), "clicked", G_CALLBACK(FrequencyCheckButton), &user_data);
     g_signal_connect(G_OBJECT(screenshot_button), "clicked", G_CALLBACK(TakeScreenshotOfDrawing), &user_data);
+    g_signal_connect(G_OBJECT(cycloides_check_button), "clicked", G_CALLBACK(CycloidesCheckButton), &user_data);
 
     gtk_widget_show_all (window);
     gtk_main ();
+
+    SHAPE_FreePoints(user_data.points_list);
+    user_data.points_list = NULL;
+    free(user_data.fft_array);
+    Pa_CloseStream(stream);
     return 0;
+}
+   
