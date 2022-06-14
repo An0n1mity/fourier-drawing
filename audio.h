5c90cb1e090d2bcc890ba9cb90b33e2fb9ce67c5
An0n1mity <49009949+An0n1mity@users.noreply.github.com>
2022-06-14 21:15:56 +0200

    Update audio.h

--- audio.h
+++ audio.h
@@ -3,39 +3,19 @@
 
 #include <portaudio.h>
 #include "fft.h"
-#define FRAME_SIZE                 1024
-/* #define SAMPLE_RATE  (17932) // Test failure to open with this value. */
+#define FRAME_SIZE    1024
 #define SAMPLE_RATE  (44100)
 #define FRAMES_PER_BUFFER (512)
 #define NUM_SECONDS     (5)
 #define NUM_CHANNELS    (2)
-/* #define DITHER_FLAG     (paDitherOff) */
-#define DITHER_FLAG     (0) /**/
-/** Set to 1 if you want to capture the recording to a file. */
-#define WRITE_TO_FILE   (0)
+
 
 /* Select sample format. */
-#if 1
 #define PA_SAMPLE_TYPE  paFloat32
 typedef float SAMPLE;
 #define SAMPLE_SILENCE  (0.0f)
 #define PRINTF_S_FORMAT "%.8f"
-#elif 1
-#define PA_SAMPLE_TYPE  paInt16
-typedef short SAMPLE;
-#define SAMPLE_SILENCE  (0)
-#define PRINTF_S_FORMAT "%d"
-#elif 0
-#define PA_SAMPLE_TYPE  paInt8
-typedef char SAMPLE;
-#define SAMPLE_SILENCE  (0)
-#define PRINTF_S_FORMAT "%d"
-#else
-#define PA_SAMPLE_TYPE  paUInt8
-typedef unsigned char SAMPLE;
-#define SAMPLE_SILENCE  (128)
-#define PRINTF_S_FORMAT "%d"
-#endif
+
 
 typedef struct {
     struct FFT_s* frequencies_array;
